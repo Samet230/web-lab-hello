fc8577b9e12ad7c96dca780dbe2a3f06e81bab7d
Samet230 <smt1shn2@gmail.com>
2026-02-24 14:10:17 +0300

    chore: initial project setup with Vite + React+ TS

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+    return (
+        <div>
+            <h1>Web Tasarimi ve Programlama</h1>
+            <h2>LAB -1</h2>
+            <p>Ad Soyad: Samet Şahin</p>
+            <p>Ogrenci No: 230541096</p>
+        </div>
+    );
+}
+
+export default App;
